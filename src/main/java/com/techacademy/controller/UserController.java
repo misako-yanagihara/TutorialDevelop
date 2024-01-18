ba4200d1be900691b850907bba976870cf1a6a62
misako-yanagihara <yanagihara-m@mainichi.co.jp>
2024-01-18 19:07:27 +0900

    Les18の課題を作成

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -56,19 +56,28 @@ public class UserController {
 
     /**User更新画面を表示*/
     @GetMapping("/update/{id}/")
-    public String getUser(@PathVariable("id") Integer id, Model model) {
-        //Modelに登録
-        model.addAttribute("user", service.getUser(id));
-        //User更新画面に遷移
-        return "user/update";
+    public String getUser(@PathVariable("id") Integer id, Model model, User user) {
+        if(id != null) {
+            model.addAttribute("user", service.getUser(id));
+            return "user/update";
+        }else {
+            model.addAttribute("user", user);
+            //User更新画面に遷移
+            return "user/update";
+        }
     }
-    /**User更新処理*/
-     @PostMapping("/update/{id}/")
-     public String postUser(User user) {
-         //User登録
-         service.saveUser(user);
-         //一覧画面にリダイレクト
-         return "redirect:/user/list";
+
+    /** User更新処理*/
+    @PostMapping("/update/(id}/")
+    public String postUser(@Validated User user, BindingResult res, Model model) {
+        if(res.hasErrors()) {
+            // エラーあり
+            return getUser(null, model, user);
+        }
+        // User登録
+        service.saveUser(user);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
     }
      /**User削除処理*/
      @PostMapping(path="list", params="deleteRun")
